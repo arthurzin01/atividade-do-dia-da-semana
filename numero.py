58b3918618abba5d03c0d589388ddc7839905728
arthurzin01 <arthur2305pr@gmail.com>
2025-06-16 11:37:02 -0300

    Add files via upload

---
+++ numero.py
@@ -0,0 +1,28 @@
+numero=int(input('diigte o dia da semana qeu eu adivinho qaul é'))
+
+if numero==1:
+ print('domingo')
+
+elif numero==2:
+ print( 'segunda')
+
+elif numero==3:
+ print( 'terça ')
+ 
+elif numero==4:
+ print('quarta')
+
+elif numero==5:
+ print('quinta')
+
+elif numero==6:
+ print('sexta')
+
+elif numero==7:
+ print('sabado')
+
+
+else: 
+  print(' se o dia for 1 é:',numero==1)
+
+
